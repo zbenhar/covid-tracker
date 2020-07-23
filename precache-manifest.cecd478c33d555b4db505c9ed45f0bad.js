8f9440e185777b711d220b8343c5f8975c2c25b1
Zakaria Benharchache <zakariabenharchache@Zakarias-MacBook-Pro.local>
2020-07-23 14:42:32 -0700

    Updates

---
+++ precache-manifest.cecd478c33d555b4db505c9ed45f0bad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34d8eb723294091e233b5dca6af0bf5",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "c8c768792af5ebb98aa4",
+    "url": "/covid-tracker/static/css/main.5b8e783f.chunk.css"
+  },
+  {
+    "revision": "37774bcb30818595ad87",
+    "url": "/covid-tracker/static/js/2.a92e261d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.a92e261d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8c768792af5ebb98aa4",
+    "url": "/covid-tracker/static/js/main.9e635b37.chunk.js"
+  },
+  {
+    "revision": "4fe0dfd05865b58ea822",
+    "url": "/covid-tracker/static/js/runtime-main.500d5b6b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/covid-19_emblem.d7265326.png"
+  }
+]);
